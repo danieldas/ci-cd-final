ef2ea99db32d4f526fd7f0784edcaf791a27f2b6
DanielOjalvo <daniel-sj77@hotmail.com>
2024-05-19 14:47:28 -0400

    docker

---
+++ docker/index.php
@@ -0,0 +1,11 @@
+ <!DOCTYPE html>
+<html lang="es">
+
+<body>
+	<center>
+<h1>Trabajo Final CI/CD</h1>
+<h2>Fecha: <?php echo date('d/m/Y')?></h2>
+<h3>Nombre: Daniel Ojalvo Canedo</h3>
+</center>
+</body>
+</html>
